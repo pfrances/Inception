607e85c4852a33aa03de8ce2c1e02c405a66e931
Pierre Franceschi <pfrances@student.42tokyo.jp>
2023-05-26 19:12:59 +0900

    still don't stuck with mariadb boot

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,6 +1,7 @@
-sleep 5
+sleep 10
+mkdir -p /run/php
 wp config create	--allow-root \
-					--dbname=$SQL_DATABASE \
-					--dbuser=$SQL_USER \
-					--dbpass=$SQL_PASSWORD \
+					--dbname=$MYSQL_DATABASE \
+					--dbuser=$MYSQL_USER \
+					--dbpass=$MYSQL_PASSWORD \
 					--dbhost=mariadb:3306 --path='/var/www/wordpress'
